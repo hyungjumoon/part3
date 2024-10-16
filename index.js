234d4998b2e0bae0c2b0fe5d9717d62e7f93fb13
Hector <hyungjumoon@gmail.com>
2024-10-16 19:17:49 -0400

    exercises 3.15-3.18, rest of 3c

--- index.js
+++ index.js
@@ -12,15 +12,17 @@ morgan.token('body', function getId (req) {
   return " "
 })
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-app.use(express.static('dist'))
 
 let persons = []
 
 app.get('/info', (request, response) => {
-  response.send(`<div><p>Phonebook has info for ${persons.length} people<p/><p>${new Date()}</p></div>`)
+  Person.countDocuments({}).then(count => {
+    response.send(`<div><p>Phonebook has info for ${count} people<p/><p>${new Date()}</p></div>`)
+  })
 })
 
 app.get('/api/persons', (request, response) => {
@@ -35,17 +37,13 @@ app.get('/api/persons/:id', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
-
-const generateId = () => {
-  const maxId = 9999999
-  return String(Math.floor(Math.random() * maxId))
-}
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -64,7 +62,38 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-const PORT = process.env.PORT || 3001
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+  next(error)
+}
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
